b5c1f2b3222586fb2611bcda28c7eda55582240b
omar jandali <omarjandali93@gmail.com>
2023-02-27 09:46:17 -0800

    completed the core challenges for the Weather API app

--- weather-api/src/App.js
+++ weather-api/src/App.js
@@ -1,12 +1,67 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css';
 
 import Weather from './Components/Weather';
 
 function App() {
+
+  const apiKey = "9040ef43483308a3e9ced80425665e1b"
+
+  const [data, setDate] = useState('')
+  const [zipcode, setZipcode] = useState('90210')
+  const [lat, setLat] = useState('')
+  const [long, setLong] = useState('')
+
+  const getWeather = () => {
+    let url = 'https://api.openweathermap.org/data/3.0/onecall?lat=' + 
+      lat + '&lon=' + 
+      long + '&appid=' + 
+      apiKey
+    try {
+      axios.get(url)
+        .then((data) => {
+          console.log(data)
+          setDate(data.data)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    } catch(error){
+      console.error(error)
+    }
+  }
+
+  const convertZipToLongLat = () => {
+    let url = "http://api.openweathermap.org/geo/1.0/zip?zip=" + zipcode + "&appid=" + apiKey
+    try {
+      axios.get(url)
+        .then((data) => {
+          console.log(data.data)
+          setLat(data.data.lat)
+          setLong(data.data.lon)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    } catch(error){
+      console.error(error)
+    }
+    console.log('sent request')
+  }
+
+  useEffect(() => {
+    long === '' ? console.log('no long') : getWeather()
+  }, [long])
+
   return (
     <div className="App">
-      <Weather />
-    </div>
+      <Weather 
+        data={data} 
+        zip={zipcode} 
+        setZipcode={setZipcode}
+        convertZipToLongLat={convertZipToLongLat}/>
+    </div>  
   );
 }
 
--- weather-api/src/Components/Weather.js
+++ weather-api/src/Components/Weather.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Weather = () => {
+const Weather = (props) => {
+  const {
+    data,
+    zip,
+    setZipcode,
+    convertZipToLongLat
+  } = props
+
+  const renderData = () => {
+    return(
+      <div>
+        Today's Temp: {data.current.temp} <br/>
+        Wind Gust: {data.current.wind_gust}<br/>
+        Wind Speed: {data.current.speed}<br/>
+        Humidity: {data.current.humidity}<br/>
+        Sunrise: {data.current.sunrise}<br/>
+        Sunset: {data.current.sunset}<br/>
+      </div>
+    )
+  }
+
+  const noData = () => {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+  
   return (
-    <div>Weather</div>
+    <div>
+      Weather
+      <form>
+        <input 
+          type="text"
+          placeholder='Enter Zip Code'
+          value={zip}
+          onChange={(e) => {setZipcode(e.target.value)}}
+        />
+      </form>
+      <div>
+        <button onClick={() => {convertZipToLongLat()}}>
+          Get Weather Data
+        </button>
+      </div>
+      {
+        data === '' ? noData() : renderData()
+      }
+    </div>
   )
 }
 
